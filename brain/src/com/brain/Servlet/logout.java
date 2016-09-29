861de608bc0ae05c75e4c34156489b287bac6f65
watermelon_R <15858540012@163.com>
2016-09-29 22:16:21 +0800

    注销补充

---
+++ brain/src/com/brain/Servlet/logout.java
@@ -0,0 +1,57 @@
+package com.brain.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class logout extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			HttpSession session = request.getSession();
+			session.removeAttribute("loginuser");          //
+			session.invalidate();                                        //
+			//RequestDispatcher requestDispatcher = request.getRequestDispatcher("../login.jsp");
+			//请求转发jsp样式消失原因：只有一次请求，最后留在最终的Servlet，而jsp中样式是根据该
+			//jsp的路径写的所以到时CSS样式路径错误找不到jsp原来对应的样式
+			//requestDispatcher.forward(request, response);
+			response.sendRedirect("../home/index.jsp");
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			doGet(request, response);
+	}
+
+}
